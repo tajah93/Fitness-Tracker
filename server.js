46923b9e1e6f01a294b60a2e7b7d4c8d40828ed4
Tajah Johnson <tajah93@hotmail.com>
2020-11-05 12:49:25 -0600

    Added and fixed html routes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const path = require("path");
 const app = express();
 
 app.use(logger("dev"));
@@ -12,21 +11,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-
-module.exports = function(app) {
-    app.get("/", function (res, req) {
-        res.sendFile(path.join(__dirname,"/public/index.html"))
-    });
-
-    app.get("/excercise", function (res, req) {
-        res.sendFile(path.join(__dirname,"/public/exercise.html"))
-    });
-
-    app.get("/stats", function (res, req) {
-        res.sendFile(path.join(__dirname,"/public/stats.html"))
-    });
-}
 
 app.listen(3060, () => {
     console.log("App running on port 3060!");
